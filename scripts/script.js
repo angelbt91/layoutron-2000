4395a2435a7dcca27d75a2cc95ccd7e62e8e0234
Angel Blanco <angelbt91@gmail.com>
2020-06-30 17:02:15 +0200

    Add img2 adding feature and its bgRect

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,7 @@
 let canvas;
+
 let imgToUpdate;
+
 let img1;
 let img1originalCoords = {
     'top': 480,
@@ -7,8 +9,8 @@ let img1originalCoords = {
     'currentWidth': 251
 };
 let img1clip = new fabric.Rect({
-    left: 219,
     top: 480,
+    left: 219,
     width: 251,
     height: 280,
     fill: 'transparent',
@@ -16,10 +18,35 @@ let img1clip = new fabric.Rect({
     absolutePositioned: true
 });
 
+let img2;
+let img2originalCoords = {
+    'top': 481,
+    'left': 728,
+    'currentWidth': 256
+};
+let img2clip = new fabric.Rect({
+    'top': 480,
+    'left': 727,
+    width: 256,
+    height: 281,
+    fill: 'transparent',
+    selectable: false,
+    absolutePositioned: true
+});
+let img2bg = new fabric.Rect({
+    'top': 480,
+    'left': 727,
+    width: 256,
+    height: 281,
+    fill: document.getElementById('bgcolor').value,
+    selectable: false
+});
+
 document.addEventListener('DOMContentLoaded', () => {
     canvas = new fabric.Canvas('c');
     canvas.setZoom(canvas.getZoom() * 0.5);
     canvas.setBackgroundImage('assets/layout-base-1.png', canvas.renderAll.bind(canvas));
+    canvas.add(img2bg);
 });
 
 function checkIfFileIsImage(url) {
@@ -28,14 +55,17 @@ function checkIfFileIsImage(url) {
 
 function setImageToUpdate(num) {
     // upon opening an URL/upload modal, we set the image number here
-    // so further functions know which image we were updating
+    // so further functions know which image we are updating
     return imgToUpdate = num;
 }
 
-function assignNewImageToGlobalVariableImage(imgX) {
-    switch(imgToUpdate) {
+function assignNewImageToGlobalVariableReference(imgX) {
+    switch (imgToUpdate) {
         case 1:
             img1 = imgX;
+            break;
+        case 2:
+            img2 = imgX;
             break;
     }
 }
@@ -50,7 +80,16 @@ function setXvalues(number) {
                 'imgXpreviewSelector': 'img1preview',
                 'imgXtoolsSelector': 'img1tools',
                 'imgXuploadBtnSelector': 'img1fileUploadBtn'
-            }
+            };
+        case 2:
+            return {
+                'imgX': img2,
+                'imgXoriginalCoords': img2originalCoords,
+                'imgXclip': img2clip,
+                'imgXpreviewSelector': 'img2preview',
+                'imgXtoolsSelector': 'img2tools',
+                'imgXuploadBtnSelector': 'img2fileUploadBtn'
+            };
     }
 }
 
@@ -80,38 +119,32 @@ function updateImg(origin) {
         imgX.scaleToWidth(newImgCoords.currentWidth);
         imgX.clipPath = imgXclip;
         canvas.add(imgX);
-        assignNewImageToGlobalVariableImage(imgX); // save imgX into the corresponding global variable
+        assignNewImageToGlobalVariableReference(imgX); // save imgX into the corresponding global variable
 
         document.getElementById(imgXpreviewSelector).src = img.src;
         document.getElementById(imgXtoolsSelector).classList.remove("d-none");
         $('#img-modal').modal('hide');
     }
 
-    switch (origin) {
-        case 'pc':
-            let route = document.getElementById(imgXuploadBtnSelector).files[0];
-            if (!checkIfFileIsImage(route.name)) {
-                return alert("Error: that doesn't look like an image. Please import a JPG, GIF or PNG.");
-            }
+    if (origin === 'pc') {
+        let file = document.getElementById(imgXuploadBtnSelector).files[0];
+        if (!checkIfFileIsImage(file.name)) {
+            return alert("Error: that doesn't look like an image. Please import a JPG, GIF or PNG.");
+        }
 
-            let reader = new FileReader();
-            reader.onload = function (event) {
-                img.src = event.target.result;
-            }
-            reader.readAsDataURL(route);
-            break;
-
-        case 'url':
-            let url = document.getElementById("imgUrlInput").value;
-            if (!checkIfFileIsImage(url)) {
-                return alert("Error: that doesn't look like an image. Please import a JPG, GIF or PNG.");
-            }
-            img.src = url;
-            break;
-
-        default:
-            console.log("Can't recognize where the image comes from.")
-            break;
+        let reader = new FileReader();
+        reader.onload = function (event) {
+            img.src = event.target.result;
+        }
+        reader.readAsDataURL(file);
+    } else if (origin === 'url') {
+        let url = document.getElementById("imgUrlInput").value;
+        if (!checkIfFileIsImage(url)) {
+            return alert("Error: that doesn't look like an image. Please import a JPG, GIF or PNG.");
+        }
+        img.src = url;
+    } else {
+        console.log("Can't recognize where the image comes from.");
     }
 }
 
@@ -121,6 +154,10 @@ function resetImg(num) {
         case(1):
             img = img1;
             imgOriginalCoords = img1originalCoords;
+            break;
+        case (2):
+            img = img2;
+            imgOriginalCoords = img2originalCoords;
             break;
     }
 
@@ -145,6 +182,11 @@ function deleteImg(num) {
             img = img1;
             imgPreview = 'img1preview';
             imgTools = 'img1tools';
+            break;
+        case(2):
+            img = img2;
+            imgPreview = 'img2preview';
+            imgTools = 'img2tools';
             break;
     }
     canvas.remove(img);
@@ -175,7 +217,10 @@ function exportImg() {
     document.body.removeChild(link);
 }
 
-
+function updateBgColor(color) {
+    img2bg.set({ fill: color });
+    canvas.renderAll();
+}
 
 
 
